536264e124f4689ecc3a3256092586c2d5ff0296
Busetty <Apuroop.Busetty@shell.com>
2020-02-11 16:43:04 +0530

    Updates

---
+++ precache-manifest.6999bcdc9030e00b6a3529e86fb9c543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9009b7a75bf4345c0effee09ee272079",
+    "url": "/Ladytron21496/calculator.git/index.html"
+  },
+  {
+    "revision": "91930180d690a2ab31fd",
+    "url": "/Ladytron21496/calculator.git/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d5064e833e6430135ca3",
+    "url": "/Ladytron21496/calculator.git/static/css/main.c00d8edb.chunk.css"
+  },
+  {
+    "revision": "91930180d690a2ab31fd",
+    "url": "/Ladytron21496/calculator.git/static/js/2.e8d47731.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Ladytron21496/calculator.git/static/js/2.e8d47731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5064e833e6430135ca3",
+    "url": "/Ladytron21496/calculator.git/static/js/main.dc37eed6.chunk.js"
+  },
+  {
+    "revision": "62de8301194c8024775f",
+    "url": "/Ladytron21496/calculator.git/static/js/runtime-main.376dff0e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee21900dc0912d0278bad2f0b66ceb53.js"
+  "/Ladytron21496/calculator.git/precache-manifest.6999bcdc9030e00b6a3529e86fb9c543.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ladytron21496/calculator.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
